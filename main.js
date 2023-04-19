6f6f6735f3a6d3ab8c67f9b7b74c9848f53524b8
dangvudinh <112934440+dangvudinh@users.noreply.github.com>
2023-04-19 14:06:43 +0700

    Update main.js

--- main.js
+++ main.js
@@ -154,10 +154,9 @@ function wrapperOnclick(){
     },timeOut*1000)
 }
 
-var audio = new Audio('./asset1/y2mate (mp3cut.net).mp3');
-audio.play();
-    // var audio = document.getElementById("audio");
-    // audio.play();
+
+     var audio = document.getElementById("audio");
+     audio.play();
   
 
 
